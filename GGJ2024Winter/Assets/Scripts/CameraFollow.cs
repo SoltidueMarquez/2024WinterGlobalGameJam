6e7417d9f4383102c4d219131a9a0106aa1d5dfb
SoltidueMarquez <2630388173@qq.com>
2024-01-24 00:20:20 +0800

    蹦床aaaaaaaaaaaaaa

--- GGJ2024Winter/Assets/Scripts/CameraFollow.cs
+++ GGJ2024Winter/Assets/Scripts/CameraFollow.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
 
+    public Text position;
     //定义两个位置进行限制相机移动
     public Vector2 minPosition;
     public Vector2 maxPosition;
@@ -29,5 +31,7 @@ public class CameraFollow : MonoBehaviour
             targetPos.z = -10f;
             transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
         }
+
+        position.text = $"{target.position.y * 100:00}m";
     }
 }
